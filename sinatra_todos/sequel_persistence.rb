3f931dbfeb27d1abec1396bb11a59565430a9566
Mata Angelou <pk60905@hotmail.com>
2017-01-13 17:29:54 +0800

    Connecting to the database once

--- sinatra_todos/sequel_persistence.rb
+++ sinatra_todos/sequel_persistence.rb
@@ -1,9 +1,10 @@
 require "sequel"
 
+DB = Sequel.connect("postgres://localhost/todos")
+
 class SequelPersistence
   def initialize(logger)
-    @db = Sequel.connect("postgres://localhost/todos")
-    @db.loggers << logger
+    DB.logger = logger
   end
 
   def find_list(id)
@@ -11,7 +12,7 @@ class SequelPersistence
   end
 
   def all_lists
-    @db[:lists].left_join(:todos, list_id: :id).
+    DB[:lists].left_join(:todos, list_id: :id).
     select_all(:lists).
     select_append do
       [ count(todos__id).as(todos_count),
@@ -22,34 +23,34 @@ class SequelPersistence
   end
 
   def create_new_list(list_name)
-    @db[:lists].insert(name: list_name)
+    DB[:lists].insert(name: list_name)
   end
 
   def delete_list(id)
-    @db[:lists].where(id: id).delete
+    DB[:lists].where(id: id).delete
   end
 
   def update_list_name(id, new_name)
-    @db[:lists].where(id: id).update(name: new_name)
+    DB[:lists].where(id: id).update(name: new_name)
   end
 
   def create_new_todo(list_id, todo_name)
-    @db[:todos].insert(list_id: list_id, name: todo_name)
+    DB[:todos].insert(list_id: list_id, name: todo_name)
   end
 
   def delete_todo_from_list(list_id, todo_id)
-    @db[:todos].where(list_id: list_id, id: todo_id).delete
+    DB[:todos].where(list_id: list_id, id: todo_id).delete
   end
 
   def update_todo_status(list_id, todo_id, new_status)
-    @db[:todos].where(list_id: list_id, id: todo_id).update(completed: new_status)
+    DB[:todos].where(list_id: list_id, id: todo_id).update(completed: new_status)
   end
 
   def mark_all_todos_as_completed(list_id)
-    @db[:todos].where(list_id: list_id).update(completed: true)
+    DB[:todos].where(list_id: list_id).update(completed: true)
   end
 
   def find_todos_for_list(list_id)
-    @db[:todos].where(list_id: list_id)
+    DB[:todos].where(list_id: list_id)
   end
 end
